9869e2eee9d0ee3273a32ca52f24b379f6fa3b94
smolyoshino <coolguygameplayer52@gmail.com>
2023-02-24 08:19:39 -0500

    it's finished

--- Game_Esports/PlayerInfo.xaml.cs
+++ Game_Esports/PlayerInfo.xaml.cs
@@ -1,4 +1,5 @@
 namespace Game_Esports;
+using MySql.Data.MySqlClient;
 
 public partial class PlayerInfo : ContentPage
 {
@@ -6,4 +7,23 @@ public partial class PlayerInfo : ContentPage
 	{
 		InitializeComponent();
 	}
+
+	private void LookupStudentInfo_Clicked(object sender, EventArgs e)
+	{
+        string cs = @"server=192.169.144.133;userid=mcctc2;password=mcctcrocks;database=sr_team_2";
+        using var con = new MySqlConnection(cs);
+        con.Open();
+        string sql = $@"SELECT * FROM UserTable WHERE PlayerID = '{LookupID.Text}';";
+        using var cmd = new MySqlCommand(sql, con);
+        using MySqlDataReader rdr = cmd.ExecuteReader();
+        while (rdr.Read())
+        {
+            FirstNameLookup.Text = rdr.GetString(1);
+            LastNameLookup.Text = rdr.GetString(2);
+            PhoneNumberLookup.Text = rdr.GetString(3);
+            DiscordIDLookup.Text = rdr.GetString(4);
+            GameLookup.Text = rdr.GetString(5);
+        }
+        con.Close();
+    }
 }
